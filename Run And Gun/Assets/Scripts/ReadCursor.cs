e9b924ebadc8a3ee660f4c1af8085127ac6f82b2
Marinus Retra <marinus@flowonline.nl>
2024-05-27 10:30:17 +0200

    ReadMouse Script Schoongemaakt

--- Run And Gun/Assets/Scripts/ReadCursor.cs
+++ Run And Gun/Assets/Scripts/ReadCursor.cs
@@ -3,17 +3,16 @@ using UnityEngine;
 public class ReadCursor : MonoBehaviour
 {
     public Texture2D CrossAir;
-    Vector2 mousePosition;
     private void Start()
     {
+        //set de texture van de muis naar de crossair
         Cursor.SetCursor(CrossAir, Vector2.zero, CursorMode.Auto);
     }
 
 
     void Update()
     {
-        mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        // pakt de muis positie en draait het wapen ernaar toe
         transform.LookAt(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector3.back);
-        transform.eulerAngles = new Vector3(0f, 0f, -transform.eulerAngles.z);
     }
 }
