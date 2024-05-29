bbf08023d22ca045a6f052c8eb092b15b0a4807d
Marinus Retra <marinus@flowonline.nl>
2024-05-29 10:48:46 +0200

    Reloading
    +Player can now reload current weapon either by pressing R or automatically upon reaching 0 ammo in magazine

--- Run And Gun/Assets/Scripts/BaseScriptable/WeaponScriptable.cs
+++ Run And Gun/Assets/Scripts/BaseScriptable/WeaponScriptable.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 public class WeaponScriptable : ScriptableObject
 {
     public int ProjectileCount;
+    public float ReloadTime;
     public string WeaponName;
     public Sprite WeaponSprite;
     public int MaxAmmoCount;
-    public int AmmoCount { get; private set; }
-    public int AmmoInMagazine { get; private set; }
+    public int AmmoCount;
+    public int AmmoInMagazine;
     public int MagazineCount;
-
+    [HideInInspector] public bool blockShoot = false;
     public WeaponScriptable(int projectileCountIn, string nameIn, Sprite spriteIn, int maxAmmoCountIn, int magazineCountIn)
     {
         ProjectileCount = projectileCountIn;
@@ -21,24 +22,43 @@ public class WeaponScriptable : ScriptableObject
         AmmoCount = MaxAmmoCount;
         AmmoInMagazine = MagazineCount;
     }
-
+    /// <summary>
+    /// Instantiate een kogel op de Gegeven Firepoint
+    /// </summary>
+    /// <param name="projectileIn"></param>
+    /// <param name="FirePoint"></param>
     public void Shoot(GameObject projectileIn, Transform FirePoint)
     {
-        GameObject Projectile = Instantiate(projectileIn, FirePoint.position, FirePoint.rotation);
-        Rigidbody2D rb = Projectile.GetComponent<Rigidbody2D>();
-        rb.AddForce(FirePoint.forward * 30, ForceMode2D.Impulse);
-        AmmoInMagazine--;
+        if (!blockShoot)
+        { 
+            GameObject Projectile = Instantiate(projectileIn, FirePoint.position, FirePoint.rotation);
+            Rigidbody2D rb = Projectile.GetComponent<Rigidbody2D>();
+            rb.AddForce(FirePoint.forward * 30, ForceMode2D.Impulse);
+            AmmoInMagazine--;
+        }
     }
-
+    /// <summary>
+    /// Zet de ammo om een reload te imiteren
+    /// </summary>
     public void Reload()
     {
-        AmmoCount -= MagazineCount;
-        AmmoInMagazine = MagazineCount;
+        if (AmmoCount < MagazineCount)
+        {
+            AmmoInMagazine = AmmoCount;
+            AmmoCount = 0;
+        }
+        else 
+        { 
+            AmmoCount -= MagazineCount;
+            AmmoInMagazine = MagazineCount;
+        }
     }
-
+    /// <summary>
+    /// Override ToString zodat ik het wapen naam en ammo count terug geef
+    /// </summary>
+    /// <returns></returns>
     public override string ToString()
     {
-        return $"{WeaponName} Ammo: {AmmoCount}";
+        return $"{WeaponName} Ammo: {AmmoInMagazine} | {AmmoCount}";
     }
-
 }
--- Run And Gun/Assets/Scripts/Gun.cs
+++ Run And Gun/Assets/Scripts/Gun.cs
@@ -1,4 +1,4 @@
-using System.Linq;
+using System.Collections;
 using UnityEngine;
 
 public class Gun : MonoBehaviour
@@ -7,12 +7,26 @@ public class Gun : MonoBehaviour
     public WeaponScriptable[] weaponIn;
     public GameObject Projectile;
     int CurrentWeapon = 0;
+
+    private void Start()
+    {
+        //zet de AmmoCount naar MaxAmmoCount van het i en doet hetzelfde met AmmoInMagazine
+        weaponIn[CurrentWeapon].blockShoot = false;
+        for (int i = 0; i < weaponIn.Length; i++)
+        {
+            weaponIn[i].AmmoCount = weaponIn[i].MaxAmmoCount;
+            weaponIn[i].AmmoInMagazine = weaponIn[i].MagazineCount;
+        }
+    }
     void Update()
     {
         CheckShoot();
         ReadSwapInput();
+        CheckReload();
     }
-
+    /// <summary>
+    /// Wisselt naar het wapen op basis van de input
+    /// </summary>
     void ReadSwapInput()
     {
         if (Input.GetKeyDown(KeyCode.Alpha1))
@@ -28,31 +42,65 @@ public class Gun : MonoBehaviour
             SwapWeapon(2);
         }
     }
-
+    /// <summary>
+    /// Wisselt je wapen en je projectile naar dat van het gezette wapen.
+    /// </summary>
+    /// <param name="weaponIn"></param>
     void SwapWeapon(int weaponIn)
     { 
       CurrentWeapon = weaponIn;
       Projectile.GetComponent<Projectile>().SetBullet(CurrentWeapon);
     }
 
+    /// <summary>
+    /// Zet de blockShoot variable van het huidige wapen en reload vervolgens het wapen
+    /// </summary>
+    /// <returns></returns>
+    public IEnumerator ReloadRoutine()
+    {
+        weaponIn[CurrentWeapon].blockShoot = true;
+        yield return new WaitForSeconds(weaponIn[CurrentWeapon].ReloadTime);
+        weaponIn[CurrentWeapon].blockShoot = false;
+        weaponIn[CurrentWeapon].Reload();
+        StopCoroutine(ReloadRoutine());
+    }
+    /// <summary>
+    /// Checkt of je kan schieten zo, ja Schiet, zo niet als alleen je magazijn leeg is reload, ammo count en magazijn is leeg, Return leeg wapen
+    /// </summary>
     void CheckShoot()
     {
+        // als je meer dan 0 ammo hebt schiet een [ProjectileCount] aantal projectiles
         if (Input.GetMouseButtonDown(0) && weaponIn[CurrentWeapon].AmmoInMagazine > 0)
         {
             for (int i = 0; i < weaponIn[CurrentWeapon].ProjectileCount; i++)
-            { 
+            {
                 weaponIn[CurrentWeapon].Shoot(Projectile, FirePoint);
                 Debug.Log(weaponIn[CurrentWeapon]);
-                // verander de positie van elke kogel voor een bullet spread effect
+                // TO-DO verander de positie van elke kogel voor een bullet spread effect
             }
         }
-        else if (Input.GetMouseButtonDown(0) && weaponIn[CurrentWeapon].AmmoInMagazine == 0)
-        {
-            weaponIn[CurrentWeapon].Reload();
-        }
-        else if (Input.GetMouseButtonDown(0) && weaponIn[CurrentWeapon].AmmoCount == 0)
+        // als je magazijn leeg is en je totale ammo 0 is dan gaat dit af
+        else if (Input.GetMouseButtonDown(0) && weaponIn[CurrentWeapon].AmmoCount < 1 && weaponIn[CurrentWeapon].AmmoInMagazine < 1)
         {
             Debug.Log("Empty Gun");
+        }
+    }
+
+     /// <summary>
+     /// Reload Wapen als je geen ammo hebt en blokkeerd de inputs van nog een reload terwijl de coroutine bezig is 
+     /// </summary>
+    void CheckReload()
+    {
+        if (Input.GetKeyDown(KeyCode.R) || weaponIn[CurrentWeapon].AmmoInMagazine < 1 && weaponIn[CurrentWeapon].AmmoCount > 0)
+        {
+            if (!weaponIn[CurrentWeapon].blockShoot)
+            {
+                StartCoroutine(ReloadRoutine());
+            }
+            else
+            {
+                Debug.Log("InReload");
+            }
         }
     }
 }
