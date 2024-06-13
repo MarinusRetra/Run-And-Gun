ddffa0c02e1013c1d0f5b935bb437890f8948cdf
Marinus Retra <marinus@flowonline.nl>
2024-06-13 13:26:47 +0200

    Added basic AIMovement

--- Run And Gun/Assets/Scripts/AIMovement.cs
+++ Run And Gun/Assets/Scripts/AIMovement.cs
@@ -14,7 +14,7 @@ public class AIMovement : MonoBehaviour
     private void Update()
     {
         //zet de velocity op basis van waar de volgende movePoint is
-        rb.velocity = new Vector2(Controller.ReturnMovementInput(MovePoints, gameObject, rb) * Controller.MoveSpeed, rb.velocity.y);
+        rb.velocity = new Vector2(Controller.ReturnMovementInput(MovePoints, transform.position) * Controller.MoveSpeed, rb.velocity.y);
 
         //Draait de ai op basis van zijn velocity
         if (rb.velocity.x >= 0.01f)
--- Run And Gun/Assets/Scripts/Controllers/AIController.cs
+++ Run And Gun/Assets/Scripts/Controllers/AIController.cs
@@ -1,3 +1,5 @@
+using System;
+using Unity.VisualScripting;
 using UnityEngine;
 
     [CreateAssetMenu(fileName = "AIController", menuName = "inputController/AIController")]
@@ -5,37 +7,38 @@ using UnityEngine;
     {
         public float JumpForce;
         public float MoveSpeed;
+        bool SwapPoint = false;
         private int currentMovePoint = 0;
-        private float savedPosition;
-        private float nextMovePointDistance;
-
+        private float nextMovePointDistance = 0.1f; //dit is hoe dichtbij de AI moet zijn voordat er een nieuwe movepoint wordt gebruikt
+        
         public bool ReturnJumpInput()
         {
             return true;
         }
 
-        public float ReturnMovementInput(GameObject[] movePointsIn, GameObject AI, Rigidbody2D rb, GameObject LastMovePoint)
+        public float ReturnMovementInput(GameObject[] movePointsIn, Vector2 AIPos)
         {
-            // currentPointX is de point waar de ai op dit moment naartoe werkt
-            float currentPointX = movePointsIn[currentMovePoint].transform.position.x;
-            // ik heb het hier aangemaakt om alles iets leesbaarder te houden
+            // zet de currentMovePoint op 0 of 1 op basis van SwapPoint
+            currentMovePoint = Convert.ToInt32(SwapPoint);
 
-            if (LastMovePoint != movePointsIn[currentMovePoint])
-            { 
-                savedPosition = currentPointX;
+            float currentMovePointX = movePointsIn[currentMovePoint].transform.position.x;
+
+            //toggled de swapoint elke keer wanneer de AI in nextMovePointDistance is
+            if (Vector2.Distance(AIPos, movePointsIn[currentMovePoint].transform.position) < nextMovePointDistance)
+            {
+                SwapPoint = !SwapPoint;   
             }
-            
-            if (currentPointX > AI.transform.position.x)
-            { 
+
+
+            //return 1 of -1 op basis van de x coordinaat van currentMovePoint
+            if (currentMovePointX < AIPos.x)
+            {
                  return -1f;
             }
-            if (currentPointX < AI.transform.position.x)
+            if (currentMovePointX > AIPos.x)
             {
                  return 1f;
             }
             return 0f;
-
-
-
-    }
+        }
     }
